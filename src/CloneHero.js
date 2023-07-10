2b7b130f04761062481d2149a253abda535d11cb
David <DavidFriend86@hotmail.com>
2023-07-10 03:55:27 -0700

    styled clone hero page

--- src/CloneHero.js
+++ src/CloneHero.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ScoreContainer from "./ScoreContainer";
+import './CloneHero.css'
 
 function CloneHero({scores, handleDelete, setScores}){
     return (
-        <div>
+        <div className='CHpage'>
+
+        <div className='CHHeader'>
         <h1>Clone Hero Records</h1>
         <img src="https://cdn2.steamgriddb.com/file/sgdb-cdn/logo/577ef1154f3240ad5b9b413aa7346a1e.png" alt="Clone Hero logo"/>
+        </div>
+        
         <ScoreContainer handleDelete={handleDelete} scores={scores} setScores={setScores}/>
         </div>
     )
