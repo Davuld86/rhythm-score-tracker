a626e8cabe549e2e3f83d1aca54b370001d1cdb7
David <DavidFriend86@hotmail.com>
2023-07-10 21:54:53 -0700

    added commas to the score for easy readibility

--- src/Score.js
+++ src/Score.js
@@ -7,7 +7,7 @@ function Score({score, deleteMode, handleDelete}){
     return(
 <div className='card'>
     <h3>{score.songName}</h3>
-    <h4>Record: {score.highScore}</h4>
+    <h4>Record: {parseInt(score.highScore).toLocaleString()}</h4>
     <VideoEmbed link={score.songLink}/>
     <h5>Recorded on: {dateFormat(score.date, 'mmm d,yyyy')}</h5>
     {deleteMode==false|| deleteMode == undefined ? null: <button onClick={()=>handleDelete(score)}>Remove this Record</button> }
